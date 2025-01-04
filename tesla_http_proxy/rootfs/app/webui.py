181900913cf3a2aafffee1e2d91924feb037e90b
Bart <stalsbart@hotmail.com>
2025-01-04 08:41:26 +0100

    Provide public key from tesla http proxy

--- tesla_http_proxy/rootfs/app/webui.py
+++ tesla_http_proxy/rootfs/app/webui.py
@@ -3,7 +3,7 @@ import logging
 import random
 import string
 from urllib.parse import urlparse, parse_qs
-from flask import cli, Flask, render_template, request
+from flask import cli, Flask, render_template, request, send_from_directory, abort
 from werkzeug.exceptions import HTTPException
 import requests
 
@@ -61,6 +61,17 @@ def index():
         scopes=SCOPES, randomstate=randomstate, randomnonce=randomnonce,
         auth_endpoint=TESLA_AUTH_ENDPOINT, ak_endpoint=TESLA_AK_ENDPOINT)
 
+@app.route('/.well-known/appspecific/com.tesla.3p.public-key.pem')
+def public_key():
+    directory = "/share/tesla"
+    filename = "com.tesla.3p.public-key.pem"
+    file_path = os.path.join(directory, filename)
+    if os.path.isfile(file_path):
+        app.logger.info(f"File {filename} exists. Serving it now.")
+        return send_from_directory(directory, filename, as_attachment=True, mimetype='application/x-pem-file')
+    else:
+        app.logger.error(f"File {filename} does not exist.")
+        return abort(404)
 
 @app.route('/callback')
 def callback():
